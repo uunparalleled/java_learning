e5180ac50122c4263ed0ffb8906cbe1b32a67407
sjj <1332518315@qq.com>
2025-01-16 20:28:26 +0800

    1

---
+++ src/leetcode/list/LRUCache.java
@@ -0,0 +1,26 @@
+package leetcode.list;
+
+/**
+ * leetcode 146. LRU 缓存
+ */
+class LRUCache {
+
+    public LRUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+        return 0;
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
